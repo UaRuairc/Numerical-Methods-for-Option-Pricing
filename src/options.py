5c3f0153cd40a0f026bada8fae632e5e168e7547
UaRuairc <uaruairc@users.noreply.github.com>
2026-09-05 10:39:16 +0100

    feat: calculate some CN greeks (Delta, Gamma, Theta)

--- src/options.py
+++ src/options.py
@@ -868,7 +868,7 @@ class EuropeanOption:
         call = self.discount(payoffs.mean(), r)
         return self.price(call, S0, r)
 
-    def price_CN(self, S0, r, sigma, s_steps=100, t_steps=500, version : str="v1"):
+    def price_CN(self, S0, r, sigma, s_steps=100, t_steps=500, version : str="v1", return_greeks=False):
 
         solvers = {
             "v1": pde_crank_nicolson,        # initial version made: unoptimised & stores V for all t_steps. Uses scipy's splu(L) for LU factorisation
@@ -882,9 +882,52 @@ class EuropeanOption:
         solver = solvers[version]
 
         V, grid = solver(self.K, S0, r, self.T, sigma, s_steps=s_steps, t_steps=t_steps)
-        call = np.interp(np.log(S0), np.log(grid['S']), V)
+
+        x = np.log(grid['S'])
+
+        call = np.interp(np.log(S0), x, V)
+
+        if return_greeks:
+            delta, gamma, theta = self._greeks_CN(V, x, sigma, r)
+            return self.price(call, S0, r), delta, gamma, theta, x
+
         return self.price(call, S0, r)
 
+    def _greeks_CN(self, V, x, sigma, r):
+        """
+        Compute the greeks by differencing the final time slice V, where V is computed by a CN method.
+        """
+        # d/dS = (1 / S) * d/dx
+        # delta = dV/dS = (1 / S) dV/dx
+        # gamma = d^2V/dS^2 = d/dS ( (1 / S) * dV/dx ) = (1/S^2) ( d^2V/dx^2 - dV/dx )
+
+        dx = x[1] - x[0]
+        Vx = (V[2:] - V[:-2]) / (2 * dx)
+        Vxx = (V[2:] - 2 * V[1:-1] + V[:-2]) / (dx) ** 2
+
+        S = np.exp(x[1:-1])  # S = exp(x)
+        delta = Vx / S
+        gamma = (Vxx - Vx) / (S ** 2)
+
+        # dv/dtau = 1/2 sigma^2 d^2V/dx^2 + (r-1/2 sigma^2)dV/dx - rV
+        # theta = - dv/dtau
+        theta = -0.5 * sigma ** 2 * Vxx - (r - 0.5 * sigma ** 2) * Vx + r * V[1:-1]
+
+        if self.type == 'put':
+            # use put call parity:
+
+            # P = C - S + e^(-r*(T-t)) * K
+
+            # d/dS P = d/dS C - 1 + 0
+            delta = delta - 1
+
+            # d/dS Delta_P = d/dS Delta_C - 0
+            # gamma = gamma
+
+            # d/dt P = d/dt C - 0 + r * e^(-r*(T-t)) * K
+            theta = theta + r * self.discount(self.K, r)
+
+        return delta, gamma, theta
 
 
 if __name__ == "__main__":
